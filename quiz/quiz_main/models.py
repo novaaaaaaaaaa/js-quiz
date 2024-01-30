a63a5a51a9b7988046cac5985ed93328f1c1657f
novaaaaaaaaaa <jackdickens048@gmail.com>
2024-01-30 13:51:33 +0000

    Create scores model and register in admin site

--- quiz/quiz_main/models.py
+++ quiz/quiz_main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Leaderboard_scores(models.Model):
+    username = models.CharField(max_length=255, null=True)
+    user_score = models.IntegerField(null=True)
